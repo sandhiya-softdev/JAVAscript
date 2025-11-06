43a3159b35b512259d2bc4dde2933c035cb97873
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-06 17:14:08 +0530

    JAVAscript commit

--- datatypes.js
+++ datatypes.js
@@ -1,2 +1,11 @@
 // what is the data types
-Data types 
+// what is data types means we set a value what kind values it holds that's data types
+// i mean different kind of values holds
+
+// data types defines the charactersitic of data.
+// a data types tells javascript what kind of data something is and what it can do
+
+
+
+
+
