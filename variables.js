76b6cf3465f62841c2d380e5b42dbf39fa94c73e
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-06 17:05:39 +0530

    JAVAscript commit

---
+++ variables.js
@@ -0,0 +1,84 @@
+// what is data types
+
+// data types defines the charactersitic of data.
+// a data types tells javascript what kind of data something is and what it can do
+
+
+// There are two types data
+// 1.Primitive data
+// 2.non Primitiva data
+
+
+// 1.Primitive data
+// there are 5 types
+// 1.Number
+// 2.string
+// 3.boolean
+// 4.undefined
+// 5.null
+
+// 2.non primitive data
+// 1.array
+// 2.object
+// 3.symbols
+
+
+// 1.number 
+
+// let a=10;
+//  b=50;
+//  console.log(a);
+
+// let a=5;
+//  console.log(a,typeof a);
+
+
+// let a=7;
+// let b=19;
+// let c=50;
+// let d=(a+b+c);
+// console.log(d);
+
+// 2.string
+
+
+// let fname=sandhiya;
+// console.log(fname);
+// this is not defined bacuse we did not give that ""quotion
+
+// let flog="sandhiya";
+// console.log(flog);
+// bacause we give this quoation then only it work
+
+
+// let firstname="dhiya"
+// console.log(firstname, (typeof firstname));
+// we can know what is the type also
+
+
+
+// let firstname="pandiya";
+// let lastname="sandhiya";
+// let fullname=firstname + lastname;
+// console.log(fullname);
+
+// 3.boolean true or false
+
+// let isstudent=true;
+// console.log(isstudent);
+
+
+// let isstudent=true;
+// console.log(isstudent, (typeof isstudent));
+
+
+
+// 4.null
+// let fname=null;
+// console.log(fname);
+// null means nothing
+
+// 5.undefined
+// let a;
+// console.log(a);
+// this is undefined because we don't give any value only declartion that'it
