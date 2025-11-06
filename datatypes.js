ba721dba54dde3f7a6af240b5a5e2723ef56be14
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-06 17:31:57 +0530

    JAVAscript commit

--- datatypes.js
+++ datatypes.js
@@ -3,7 +3,81 @@
 // i mean different kind of values holds
 
 // data types defines the charactersitic of data.
-// a data types tells javascript what kind of data something is and what it can do
+// a data types tells javascript what kind of data something is and what it can dot
+
+
+// there are two types of the data types
+// 1. primitive data types
+// 2 non primitive data types
+
+
+// 1.primitive data types
+// 1. string
+// 2. number
+// 3.boolean
+// 4.null
+// 5. undefined
+
+
+// 2. non-primitive data types
+
+// 1. array
+// 2. object
+
+
+
+// 1.primitive data types
+
+// 1.string
+
+
+// var a=sandhiya;
+// var b=dhiya;
+// console.log(b);
+// because the we did not give that quoation
+
+
+
+// let a="sandhiya";
+// let b="dhiya";
+// console.log(b);
+
+// let a="fresher";
+// let b="developer";
+// let c="it";
+// console.log(a+b+c);
+
+//  let a="bts";
+//  let b="v37863";
+//  console.log(b);
+// because  we can give that number also in the string
+
+
+
+// const a="vim"
+// console.log(a,(typeof a));
+// we can know that which type of  the we are using in this datatypes
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
 
 
 
