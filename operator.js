536ffcc71db276c55a2554648b2b7f5d73f863e8
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-07 17:25:59 +0530

    JAVAscript commit

--- operator.js
+++ operator.js
@@ -2,3 +2,7 @@
 // A javascript operator is a symbol or keyword that tells the computer to perform a
 // an operation on one or more vales.
 
+// There are three operator
+// 1.arithmetic operator
+
+
