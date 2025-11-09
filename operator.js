9b843e5cb03d8633715350eb2882762e7c396d13
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-09 16:30:06 +0530

    JAVAscript commit

--- operator.js
+++ operator.js
@@ -141,15 +141,16 @@
 // let b=10;
 // console.log(a<b);
 
+// greather than equal
 
 // let a=10;
 // let b=20;
 // console.log(a>=b);
 
 
-let a=10;
-let b=10;
-console.log(a>=b);
+// let a=10;
+// let b=10;
+// console.log(a>=b);
 
 
 
