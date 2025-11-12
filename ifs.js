f40360f16fea9818255115ff65ed0e9155af5e89
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-12 15:14:41 +0530

    JAVAscript commit

--- ifs.js
+++ ifs.js
@@ -426,7 +426,13 @@ let avg=87;
 if(avg>=90 && avg<=100){
     console.log("wait");
     
-} else if(avg>)
+} else if(avg>=80 && avg<=89){
+    console.log("come on")
+}else if(avg>=70 && avg<=79){
+
+}else(avg>=70){
+    log
+}
 
 
 
