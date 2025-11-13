425ad8e66f89512ab071a2319455cd904ca3be2e
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-13 12:21:48 +0530

    JAVAscript commit

---
+++ loop.js
@@ -0,0 +1,16 @@
+// This is why we are using loop 
+// when you list of loop that's why we are using loop
+
+
+// ex
+// bulk of output
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+
+
+
+
+
