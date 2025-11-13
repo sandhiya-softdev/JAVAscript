3eab4e42e2d9c23352df3f24d93a3ac6a6eebad5
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-13 21:27:09 +0530

    JAVAscript commit

--- loop.js
+++ loop.js
@@ -10,6 +10,88 @@
 // console.log("4");
 // console.log("5");
 
+// that's why they created the loop
+// there are four types of lopp
+// for loop
+// while loop
+// do-white loop
+// for-loop
+// for-in loop
+
+// syntax
+// for(initialexpression;CSSCondition;step){
+//     console.log("what you want you can print");
+    
+// }THIS IS SYNTAX OF THE FOR LOOP
+
+
+
+// // let a=1;intialization
+// // for(a=1;a<=10;a++){first intialization second condition, step
+//     console.log(a);
+    
+// }
+
+
+// let a=10;
+
+// for(a=10;a>6;a--){
+//     console.log("number")
+// }
+
+// let i=1;
+
+// for(i=1;i<=10;i++){
+//     console.log(i);
+    
+// }
+// how it works
+// for(i=1;1<=10;1++){
+// console.log(i);
+// }
+// i=2;
+// for(i=2;2<=10;2++)
+// console.log(i);
+
+// i=3;
+// for(i=3;3<=10;3++)
+//     console.log(i);
+
+// i=4;
+// for(i=4;4<=10;4++)
+//     console.log(i);
+
+// i=5;
+// for(i=5;5<=10;5++)
+//     console.log(i);
+// let a=1;
+// for(a=1;a<=5;a++){
+//     console.log(a);
+    
+// }
+
+// print all the numbers from 1 to 10 using for loop
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
