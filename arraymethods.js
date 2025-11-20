b0010255ca86cb31951879105768556e03962555
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-20 17:41:05 +0530

    JAVAscript commit

--- arraymethods.js
+++ arraymethods.js
@@ -1,15 +1,100 @@
-const myArray=[];
-// adding element
+// const myArray=[];
+// // adding element
 
-myArray[0]="sandhiya";
-myArray[1]="pandiyan";
-myArray[2]="23"
-myArray[3]=true
-console.log(myArray);
+// myArray[0]="sandhiya";
+// myArray[1]="pandiyan";
+// myArray[2]="23"
+// myArray[3]=true
+// console.log(myArray);
 
-// HOW TO CALCULATE LENGTH
-console.log(myArray.length)
+// // HOW TO CALCULATE LENGTH
+// console.log(myArray.length)
 
-// indi
+// // indididual calculate we can do it
+
+// console.log(myArray[0]);
+// console.log(myArray[1]);
+// console.log(myArray[2]);
+// console.log(myArray[3]);
+// console.log(myArray[4])
+
+// // we can also use this multiple elements how to find the length of the
+// console.log(myArray[myArray.length-1])
+
+// Adding the element the last things
+// push practice
+
+// let myArray=["sandhiya","pandiyan", "bommi"];
+// myArray.push("jagatheesh");
+// console.log(myArray);
+
+// let things=["sofa", "piro","fan"];
+// things.push("mobile");
+// console.log(things);
+
+// let sandhiya=["loyal", "care","frnd"];
+// sandhiya.push("lover")
+// console.log(sandhiya);
+
+// let xerox=["xerox", "print","lamination"];
+// xerox.push("color xerox");
+// console.log(xerox);
+
+
+// let animals=["ranbir","rashmika"];
+// animals.pop();
+// console.log(animals);
+
+
+
+// let animals=["sandhiya","ram"];
+// animals.pop();
+// console.log(animals);
+
+
+//We are using first elemenet we add something
+
+// let a=[2,3,4,5,6];
+// a.unshift(1);
+// console.log(a);
+
+
+// let birds=["pura", "kakka","arabic"];
+// console.log(birds);
+
+// let krishna=["cheap", "funny"];
+// console.log(krishna);
+
+// we want to add something in front we will use this unshift
+// let sandhiya=["poor girl","funny"];
+// sandhiya.unshift("cravings for love");
+// console.log(sandhiya);
+
+// shift means we want removes that things
+// let a=[1,2,3,4,5];
+// a.shift();
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
