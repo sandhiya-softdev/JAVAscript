73c81cd6ed16a4635b044f87c41847ed2f387a8e
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-21 21:24:26 +0530

    JAVAscript commit

---
+++ object.js
@@ -0,0 +1,54 @@
+// let person={
+//     name:"sandhuya",
+//     age:2,
+//     gender:"female"
+
+// }
+// console.log(person);
+
+
+// let things={
+//     sister:"bhuvana",
+//     borther: "kalao",
+
+
+// }
+// console.log(things);
+
+
+// let laptop={
+//     tvs:"honda",
+//     mobile:"oppo"
+// }
+// console.log(laptop);
+
+
+// HOW TO ADD TO SOMEONE
+// let things={
+//     game:"boo",
+//     high:"speex"
+// }
+// things.rean="tj"
+// console.log(things);
+
+
+
+// let on={
+//     airplane:"air",
+//     car:"car speed"
+// }
+// on.vehices="scooty"
+// console.log(on);
+
+// we can change the property
+// let on={
+//      airplane:"air",
+//      car:"car speed"
+// }
+// on.airplane="things";
+// console.log(on);
+
+
+// delete the property
+
+
