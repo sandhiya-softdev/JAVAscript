5343138c32b839d6771d8adf92633a78ec583820
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-26 15:17:57 +0530

    JAVAscript commit

--- array.js
+++ array.js
@@ -12,9 +12,22 @@
 // let stack=["html","css","javascript"];
 // stack.unshift("full");
 // console.log(stack);
-/ let stack=["html","css","javascript"];
-// stack.unshift("full");
-// console.log(stack);
+
+//  let stack=["morn","html","css","javascript"];
+// stack.shift();
+//  console.log(stack);
+
+// let todo=["early wakeup", "drinkwater","washroom"];
+// todo.push("ammaku help", "cloth wash", "take bath"); 
+// console.log(todo);
+
+// let todo= ["wake up", "drink water", "washroom"];
+// todo.forEach(todo=>{
+//     console.log(todo);
+    
+
+// });
+
 
 
 
