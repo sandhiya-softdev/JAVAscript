3db2567d6fe8ae77ef6e90ee0186099456b63df0
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-11-26 13:39:00 +0530

    JAVAscript commit

---
+++ array.js
@@ -0,0 +1,23 @@
+// let movies=["dude", "premalu","nithane enponvasatham","alaipayuthy","ok kanmani"];
+// console.log(movies[1]);
+
+// let xerox=["color", "printout"];
+// xerox.push("scan");
+// console.log(xerox);
+
+// let xerox=["color", "printout"];
+// xerox.pop();
+// console.log(xerox);
+
+// let stack=["html","css","javascript"];
+// stack.unshift("full");
+// console.log(stack);
+/ let stack=["html","css","javascript"];
+// stack.unshift("full");
+// console.log(stack);
+
+
+
+
+
+
