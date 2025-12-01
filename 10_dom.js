641b6a83c02c297d0197ba3141ac81deae3955e7
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-01 15:45:58 +0530

    JAVAscript commit

---
+++ 10_dom.js
@@ -0,0 +1,25 @@
+// First things doument.
+// second things what you want that element that getElementbyid
+// what we change and we have to show that' we give id name
+
+var things=document.getElementById("one");
+console.log(things.textContent);
+things.textContent="dom";
+
+var para=document.getElementById("title");
+console.log(para.textContent);
+para.textContent="Welcome";
+
+var on=document.getElementById("info");
+console.log(on.textContent);
+on.textContent="Updated paragraph"
+
+var yet=document.getElementById("btn");
+console.log(yet.textContent);
+yet.textContent="submit"
+
+var high=document.getElementById("colorText");
+console.log(high.textContent);
+high.textContent="green"
+
+
