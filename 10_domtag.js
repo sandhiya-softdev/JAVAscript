fbf94388da3c40b298af36c2f0aac0ebb5af9915
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-04 16:29:14 +0530

    JAVAscriipt commit

--- 10_domtag.js
+++ 10_domtag.js
@@ -15,12 +15,32 @@
 // even index-green
 // odd index-blue
 
-var on=document.getElementsByTagName("li");
-console.log(on);
-for(var i=0;i<on.length;i++);{
-if(i%2==0){
-    on[i].style.color="green";
-}else{
-    on[i].style.color="blue"
-}
-}
+// var on=document.getElementsByTagName("li");
+// console.log(on);
+// for(var i=0;i<on.length;i++);{
+// if(i%2==0){
+//     on[i].style.color="green";
+// }else{
+//     on[i].style.color="blue"
+// }
+// }
+
+
+// var high=document.getElementsByTagName("p");
+// console.log(high);
+// for(var i=0;i<high.length;i++){
+//     high[i].style.color="red";
+// }
+
+
+// var re=document.getElementsByTagName("h3");
+// console.log(re);
+// re[0].style.color="blue";
+// re[1].style.color="green";
+// re[2].style.color="orange";
+
+
+
+document
+
+
