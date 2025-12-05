fa9bac5786f1831fd040a85c55d58471bbbf0e7d
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-05 17:52:47 +0530

    JAVAscript commit

---
+++ 11_returnfun.js
@@ -0,0 +1,16 @@
+// function double(n){
+//     return n*3;
+// }
+// console.log(double(2));
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,2));
+
+
+function square(n){
+    r
+}
+
+
