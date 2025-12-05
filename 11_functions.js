cef3bb17b01508b696b3bc6f2f6ecd5fa5800129
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-05 15:53:20 +0530

    JAVAscript commit

--- 11_functions.js
+++ 11_functions.js
@@ -4,4 +4,30 @@ function hello(){
 }
 hello();
 
+// print "welcome sandhiya" 3 times
+
+function welcome(){
+    console.log("welcome sandhiya")
+}
+welcome();
+welcome();
+welcome();
+
+// print your favourite color
+
+
+function color(){
+    console.log("My fav color is purple")
+}
+color();
+
+// say good morning
+
+function morning(){
+    console.log("Good morning")
+}
+morning();
+
+
+
 
