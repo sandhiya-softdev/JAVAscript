241b21fcc1735651d1df94572026900a4d3c77a0
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-05 18:07:38 +0530

    JAVAscript commit

--- 11_returnfun.js
+++ 11_returnfun.js
@@ -20,4 +20,36 @@
 // console.log(fullName("sandhiya","p"));
 
 
+// function checkage(age){
+//     return age>=18;
+// }
+// console.log(checkage(20));
+// console.log(checkage(10));
+
+
+// write a function double(n)
+
+// function double(n){
+//     return n*2;
+// }
+// console.log(double(2));
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(12,14));
+
+
+// function square(n){
+//     return n*n;
+// }
+
+// console.log(square(4));
+
+
+
+
+
+
 
