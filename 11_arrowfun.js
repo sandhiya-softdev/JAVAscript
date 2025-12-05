4fc21a835446a8d5e0dad0f119e5a20d99ea9b95
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-05 20:22:26 +0530

    JAVAscript commit

--- 11_arrowfun.js
+++ 11_arrowfun.js
@@ -1,3 +1,28 @@
 // variables
 // functionname
 // code block
+
+
+// const things=()=>{
+//     console.log("hey");
+    
+// }
+// things();
+
+// let high=(name)=>{
+//     console.log("hello"+name);
+    
+// }
+// high("sandhiya");
+
+
+// const double=(n)=>{
+//     console.log(n*2);
+    
+// }
+// double(4);
+
+// var add=(a,b)=>{
+//     return a+b;
+// }
+// console.log((add(3,6)));
