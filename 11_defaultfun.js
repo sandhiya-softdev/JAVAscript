2829edda8bcdc00622a44baa93e22b771b0a42cd
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-05 19:53:50 +0530

    JAVAscript commit

--- 11_defaultfun.js
+++ 11_defaultfun.js
@@ -4,3 +4,41 @@
 // }
 // greet("sandhiya");
 // greet();
+
+//
+// function high(name="hello"){
+//     console.log("hey there"+name+"p");
+    
+// }
+// high("sandhiya");
+// high();
+
+
+// function things(name="biro"){
+//     console.log("fridge"+name);
+    
+// }
+// things();
+// things("on");
+
+// function animals(name="cats"){
+//     console.log("dogs"+name);
+    
+// }
+// animals("kutta");
+// animals();
+
+
+// function gym(name="protein"){
+//     console.log("hey there"+name);
+    
+// }
+// gym();
+// gym("egg")
+
+
+// function welcome(name="guest"){
+//     console.log("welcome"+name);
+    
+// }
+// welcome();
---
+++ 11_expressionfun.js
@@ -0,0 +1,49 @@
+// systax
+//  var
+// function name
+// keyword
+
+// let greet=function(name){
+//     console.log("sandhiya"+name);
+    
+// }
+// greet("fun");
+// greet("things")
+
+
+// let double=function(n){
+//     return n*2;
+// };
+// console.log(double(9));
+
+// const add=function(a,b){
+//     return(a+b);
+// }
+// console.log(add(2,5));
+//  const greet=function(name){
+//     return "hello "+name;
+//  }
+//  console.log(greet("dhiya"));
+
+//  const square=function(n){
+//     return n*n;
+// //  }
+// //  console.log(square(2));
+
+// const evenOdd=function(num){
+//     if(num%2==0){
+//         return "even";
+//     }else{
+//         return "odd"
+//     }
+// }
+// console.log(evenOdd(7));
+
+
+const bigger=function(a,b){
+    
+}
+ 
+ 
+
+
