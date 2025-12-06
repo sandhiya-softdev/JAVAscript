71612d7dac5def3238e22ceda21bb62c7abff3d8
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-06 13:19:33 +0530

    JAVAscript commit

---
+++ 12_blockscope.js
@@ -0,0 +1,16 @@
+// {
+//     let name="sandhiya";
+//     console.log(name);
+    
+// }
+
+// {
+//     const color="pink";
+//     console.log(color);
+    
+// }
+
+// for(let i=0;i<3;i++){
+//     console.log(i);
+    
+// }
---
+++ 12_funscope.js
@@ -0,0 +1,20 @@
+function greet(){
+    let msg="hello";
+    console.log(msg);
+    
+}
+greet();
+
+// function add(){
+//     let a=90;
+//     let b=90;
+//     console.log(a+b);
+// }
+// add();
+
+
+function showName(){
+    let name="sandhiya";
+    console.log("my name is"+ name);
+}
+showName();
