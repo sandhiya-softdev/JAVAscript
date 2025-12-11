9b8b263452f29773691bad791688ff99242d5400
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-11 20:13:10 +0530

    JAVAscript commit

--- 10_domstyle.js
+++ 10_domstyle.js
@@ -1,78 +1,82 @@
-var on=document.getElementById("colorText");
-console.log(on.style.color);
-on.style.color="red";
+// var on=document.getElementById("colorText");
+// console.log(on.style.color);
+// on.style.color="red";
 
-var on=document.getElementById("one");
-console.log(on.style.color);
-on.style.color="green";
-
-
-var things=document.getElementById("head");
-console.log(things.style.color);
-things.style.color="green";
-
-var hope=document.getElementById("line");
-console.log(hope.style.color);
-hope.style.color="orange";
+// var on=document.getElementById("one");
+// console.log(on.style.color);
+// on.style.color="green";
 
 
-var rate=document.getElementById("topic");
-console.log(rate.style.color);
-rate.style.color="purple";
+// var things=document.getElementById("head");
+// console.log(things.style.color);
+// things.style.color="green";
+
+// var hope=document.getElementById("line");
+// console.log(hope.style.color);
+// hope.style.color="orange";
 
 
-var op1=document.getElementById("a");
-console.log(op.style.color);
-op1.style.color="red";
-
-var op2=document.getElementById("b");
-console.log(op.style.color);
-op2.style.color="yellow";
+// var rate=document.getElementById("topic");
+// console.log(rate.style.color);
+// rate.style.color="purple";
 
 
-// change the colors
+// var op1=document.getElementById("a");
+// console.log(op.style.color);
+// op1.style.color="red";
 
-var vv=document.getElementById("msg");
-console.log(vv.style.color);
-vv.style.color="blue";
-
-
-var gh=document.getElementById("head");
-console.log(gh.style.fontSize);
-gh.style.fontSize="40px";
-gh.style.color="green"
-
-// change background color
-
-var yy=document.getElementById("box");
-console.log(yy.style.background);
-yy.style.background="yellow";
-yy.style.padding="20px";
+// var op2=document.getElementById("b");
+// console.log(op.style.color);
+// op2.style.color="yellow";
 
 
-// change the border 2px solid black;
+// // change the colors
 
-var gn=document.getElementById("para");
-console.log(gn.style.border);
-gn.style.border="2px solid red";
-
-// level 2 change two elements styles differently
-var live=document.getElementById("p1");
-console.log(live.style.color);
-live.style.color="green";
+// var vv=document.getElementById("msg");
+// console.log(vv.style.color);
+// vv.style.color="blue";
 
 
-var live1=document.getElementById("p2");
-console.log(live1.style.color);
-live1.style.color="purple";
+// var gh=document.getElementById("head");
+// console.log(gh.style.fontSize);
+// gh.style.fontSize="40px";
+// gh.style.color="green"
+
+// // change background color
+
+// var yy=document.getElementById("box");
+// console.log(yy.style.background);
+// yy.style.background="yellow";
+// yy.style.padding="20px";
 
 
-// change width and height
+// // change the border 2px solid black;
 
-var re=document.getElementById("smallBox");
-console.log(re.style.width);
-re.style.width="150px";
-re.style.height="150px";
-re.sstyle.background="pink";
+// var gn=document.getElementById("para");
+// console.log(gn.style.border);
+// gn.style.border="2px solid red";
+
+// // level 2 change two elements styles differently
+// var live=document.getElementById("p1");
+// console.log(live.style.color);
+// live.style.color="green";
+
+
+// var live1=document.getElementById("p2");
+// console.log(live1.style.color);
+// live1.style.color="purple";
+
+
+// // change width and height
+
+// var re=document.getElementById("smallBox");
+// console.log(re.style.width);
+// re.style.width="150px";
+// re.style.height="150px";
+// re.sstyle.background="pink";
+
+var things=document.getElementById("dom")
+console.log(things.textContent);
+things.textContent="Doument object model";
 
 
