79d2be7c2487507a4c4ab15bcfaac24742e63cde
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-12 15:53:11 +0530

    JAVAscript commit

--- 10_domclass.js
+++ 10_domclass.js
@@ -3,4 +3,17 @@
 //     things[i].textContent="good";
 //  }
 
+// var output = document.getElementsByClassName("on");
+
+// output[0].textContent = "very bad";
+// output[1].style.color = "grey";
+// output[2].style.width = "600px";
+
+// var things=document.getElementsByClassName("bt");
+// i=0;
+// while (i<things.length) {
+//     things[i].textContent="hello";
+//     i++;
+// }
+
 
