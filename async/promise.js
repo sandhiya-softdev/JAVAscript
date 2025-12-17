cd80aadabd540da7e76d790d29a22f30ca49bf36
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2025-12-17 22:49:02 +0530

    JAVAscript commit

--- async/promise.js
+++ async/promise.js
@@ -1,1 +1,7 @@
-a 
+// A promise will either resolve with a value or reject with an error
+// syntax
+// let promise=variables
+// new promise=javascript default
+// parameter(resolve, reject)
+// if condition true ah irunthuchina resolve
+// other reject and with an error also
